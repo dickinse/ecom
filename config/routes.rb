abc70122920168e683d626366961bc8fb4dc2356
dickinse <isitheavierthanair@gmail.com>
2015-12-16 14:01:46 -0500

    Changed Routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
   get 'products/index'
 
-#  resources :products, 
-resources :orders, only: [:index, :show, :create, :destroy]
+  resources :products 
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
