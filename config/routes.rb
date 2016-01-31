860e702e2554b57f2fe2d31fbe6a533d72f065c6
dickinse <isitheavierthanair@gmail.com>
2016-01-31 18:36:59 -0500

    Now with books, and pizazz

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'static_pages#index'
+   root 'products#index'
 
   #Orders Resources   
   # Example of regular route:
