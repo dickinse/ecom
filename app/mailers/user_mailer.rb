5594456acf5dbd9cfb75e538902036d53079f428
dickinse <isitheavierthanair@gmail.com>
2016-02-01 15:19:52 -0500

    added delivery notice static

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,6 @@ class UserMailer < ApplicationMailer
       @message = message
       mail(:from => email,
          :to => "isitheavierthanair@gmail.com",
-         :subject => "A new contact form message from #{name}").deliver_now
+         :subject => "A new contact form message from #{name}")
    end
 end
